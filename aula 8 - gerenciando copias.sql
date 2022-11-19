18e169fff2b5735bb065a65d9643c6f4cef65e1d
Sweyd Manaf <abdulsweyd@gmail.com>
2022-11-19 20:54:53 +0000

    Create aula 8 - gerenciando copias.sql

---
+++ aula 8 - gerenciando copias.sql
@@ -0,0 +1,13 @@
+USE cadastro;
+SELECT * FROM cursos;
+SELECT * FROM gafanhotos;
+DESCRIBE gafanhotos;
+DESC cursos;
+DROP DATABASE cadastro;
+
+USE cadastro;
+SHOW TABLES;
+DESCRIBE gafanhotos;
+
+SELECT * FROM gafanhotos;
+SELECT * FROM cursos;
